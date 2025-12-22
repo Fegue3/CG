7b25ce7b9f7e3a1c7152eb243ec73e45266782d2
imdtcode <cardosodtc@hotmail.com>
2025-12-22 23:23:27 +0000

    Refactor Shader class to improve error handling and streamline shader loading process

--- Breakout3D/Breakout3D/src/engine/Shader.cpp
+++ Breakout3D/Breakout3D/src/engine/Shader.cpp
@@ -1,47 +1,129 @@
 #include "engine/Shader.hpp"
+#include <GL/glew.h>
+
 #include <fstream>
 #include <sstream>
-#include <string>
+#include <iostream>
 
-static std::string readFile(const char* path) {
+namespace engine {
+
+static std::string loadFile(const std::string& path) {
     std::ifstream f(path);
+    if (!f.is_open()) {
+        std::cerr << "Failed to open shader file: " << path << "\n";
+        return {};
+    }
     std::stringstream ss;
     ss << f.rdbuf();
     return ss.str();
 }
 
-static GLuint compile(GLenum type, const char* src) {
-    GLuint s = glCreateShader(type);
-    glShaderSource(s, 1, &src, nullptr);
-    glCompileShader(s);
-    return s;
+static bool checkShader(GLuint s, const char* label) {
+    GLint ok = 0;
+    glGetShaderiv(s, GL_COMPILE_STATUS, &ok);
+    if (!ok) {
+        GLint len = 0;
+        glGetShaderiv(s, GL_INFO_LOG_LENGTH, &len);
+        std::string log(len, '\0');
+        glGetShaderInfoLog(s, len, &len, log.data());
+        std::cerr << "Shader compile failed (" << label << "):\n" << log << "\n";
+        return false;
+    }
+    return true;
 }
 
-namespace engine {
+static bool checkProgram(GLuint p) {
+    GLint ok = 0;
+    glGetProgramiv(p, GL_LINK_STATUS, &ok);
+    if (!ok) {
+        GLint len = 0;
+        glGetProgramiv(p, GL_INFO_LOG_LENGTH, &len);
+        std::string log(len, '\0');
+        glGetProgramInfoLog(p, len, &len, log.data());
+        std::cerr << "Program link failed:\n" << log << "\n";
+        return false;
+    }
+    return true;
+}
 
-bool Shader::load(const char* vertPath, const char* fragPath) {
-    std::string vs = readFile(vertPath);
-    std::string fs = readFile(fragPath);
+Shader::Shader() {}
 
-    GLuint v = compile(GL_VERTEX_SHADER, vs.c_str());
-    GLuint f = compile(GL_FRAGMENT_SHADER, fs.c_str());
+Shader::~Shader() {
+    destroy();
+}
 
-    m_program = glCreateProgram();
-    glAttachShader(m_program, v);
-    glAttachShader(m_program, f);
-    glLinkProgram(m_program);
+bool Shader::load(const std::string& vertPath, const std::string& fragPath) {
+    destroy();
+
+    std::string vs = loadFile(vertPath);
+    std::string fs = loadFile(fragPath);
+    if (vs.empty() || fs.empty())
+        return false;
+
+    const char* vsrc = vs.c_str();
+    const char* fsrc = fs.c_str();
+
+    GLuint v = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(v, 1, &vsrc, nullptr);
+    glCompileShader(v);
+    if (!checkShader(v, "vertex")) {
+        glDeleteShader(v);
+        return false;
+    }
+
+    GLuint f = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(f, 1, &fsrc, nullptr);
+    glCompileShader(f);
+    if (!checkShader(f, "fragment")) {
+        glDeleteShader(v);
+        glDeleteShader(f);
+        return false;
+    }
+
+    m_id = glCreateProgram();
+    glAttachShader(m_id, v);
+    glAttachShader(m_id, f);
+    glLinkProgram(m_id);
 
     glDeleteShader(v);
     glDeleteShader(f);
 
-    return m_program != 0;
+    if (!checkProgram(m_id)) {
+        destroy();
+        return false;
+    }
+
+    return true;
+}
+
+void Shader::use() const {
+    glUseProgram(m_id);
 }
 
 void Shader::destroy() {
-    if (m_program) glDeleteProgram(m_program);
-    m_program = 0;
+    if (m_id != 0) {
+        glDeleteProgram(m_id);
+        m_id = 0;
+    }
 }
 
-void Shader::use() const { glUseProgram(m_program); }
+unsigned int Shader::id() const {
+    return m_id;
+}
+
+void Shader::setMat4(const std::string& name, const glm::mat4& mat) const {
+    GLint loc = glGetUniformLocation(m_id, name.c_str());
+    if (loc >= 0) glUniformMatrix4fv(loc, 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::setVec3(const std::string& name, const glm::vec3& v) const {
+    GLint loc = glGetUniformLocation(m_id, name.c_str());
+    if (loc >= 0) glUniform3fv(loc, 1, &v[0]);
+}
+
+void Shader::setFloat(const std::string& name, float v) const {
+    GLint loc = glGetUniformLocation(m_id, name.c_str());
+    if (loc >= 0) glUniform1f(loc, v);
+}
 
 } // namespace engine
